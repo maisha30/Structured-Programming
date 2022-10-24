839d8cc60482d6b69ef892a62d05886252c6c005
maisha30 <112333752+maisha30@users.noreply.github.com>
2022-10-24 23:58:41 +0600

    Add files via upload

---
+++ lab task 5/2.ASCII converter.c
@@ -0,0 +1,14 @@
+/*This C PROGRAM to convert ASCII code.
+Author: Maisha Rahman, */
+
+#include<stdio.h>
+int main() {
+
+int i;
+
+for(i=0;i<=255;i++)
+{
+    printf(" The ascii value is %c = %d\n", i, i);
+}
+return 0;
+}
